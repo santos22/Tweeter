a2a5666230793795d3e776cdbf0f8978457b956a
santos22 <santosjavier22@gmail.com>
2016-02-14 20:15:53 -0600

    Modified AppDelegate to retrieve access token

--- Tweeter/Tweeter/AppDelegate.swift
+++ Tweeter/Tweeter/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +40,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        TwitterClient.sharedInstance.fetchAccessTokenWithPath(
+            "oauth/access_token", method: "POST", requestToken: BDBOAuth1Credential (queryString: url.query), success: { (accessToken:
+                BDBOAuth1Credential!) -> Void in
+                print("Got the access token!")
+
+            }) { (error: NSError!) -> Void in
+                print("Failed to receive access token")
+        }
+        return true
     }
 
 
--- Tweeter/Tweeter/ViewController.swift
+++ Tweeter/Tweeter/ViewController.swift
@@ -28,8 +28,11 @@ class ViewController: UIViewController {
             "oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterdemo://oauth"), scope: nil, success: { (requestToken:
                 BDBOAuth1Credential!) -> Void in
                 print("Got the request token")
+                let authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+                
+                UIApplication.sharedApplication().openURL(authURL!)
         }) { (error: NSError!) -> Void in
-            print("Failed to get requet token")
+            print("Failed to get request token")
         }
     }
 
